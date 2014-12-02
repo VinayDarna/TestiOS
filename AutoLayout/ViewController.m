2a0cd28f6e3fe5fce1046b8df08a93fbe6beb9f3
VinayKumarDarna <OliveTech@Apples-iMac.local>
2014-12-02 12:13:43 +0530

    Initial Commit

---
+++ AutoLayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayout
+//
+//  Created by Olive Tech on 02/12/14.
+//  Copyright (c) 2014 Olive Tech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
